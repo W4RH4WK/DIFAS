8e81180ef7ad7acadf5f283f059bfe25910c1d86
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2018-03-30 13:18:53 +0200

    Add jemalloc

---
+++ packages/jemalloc.sh
@@ -0,0 +1,7 @@
+NAME="jemalloc"
+VERSION="5.0.1"
+PACKAGE="$NAME-$VERSION"
+
+FILE="$PACKAGE.tar.bz2"
+URL="https://github.com/jemalloc/jemalloc/releases/download/$VERSION/$FILE"
+SHA256SUM="4814781d395b0ef093b21a08e8e6e0bd3dab8762f9935bbfb71679b0dea7c3e9"
