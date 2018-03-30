7d6773e1ed1876c8d9693d77de88a44fdecd783b
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2018-03-30 12:57:15 +0200

    Add zlib

---
+++ packages/zlib.sh
@@ -0,0 +1,7 @@
+NAME="zlib"
+VERSION="1.2.11"
+PACKAGE="$NAME-$VERSION"
+
+FILE="$PACKAGE.tar.gz"
+URL="https://zlib.net/$FILE"
+SHA256SUM="c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
