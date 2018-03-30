f258b82f1c9766283a3cef4a10173b5b1b13707a
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2018-03-30 12:55:22 +0200

    Add Flex / Bison

---
+++ packages/bison.sh
@@ -0,0 +1,7 @@
+NAME="bison"
+VERSION="3.0.4"
+PACKAGE="$NAME-$VERSION"
+
+FILE="$PACKAGE.tar.gz"
+URL="https://ftp.gnu.org/gnu/bison/$FILE"
+SHA256SUM="b67fd2daae7a64b5ba862c66c07c1addb9e6b1b05c5f2049392cfd8a2172952e"
---
+++ packages/flex.sh
@@ -0,0 +1,7 @@
+NAME="flex"
+VERSION="2.6.4"
+PACKAGE="$NAME-$VERSION"
+
+FILE="$PACKAGE.tar.gz"
+URL="https://github.com/westes/flex/releases/download/v$VERSION/$FILE"
+SHA256SUM="e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
