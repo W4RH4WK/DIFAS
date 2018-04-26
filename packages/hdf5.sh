266b5eaaffdf2620b7daee942f96235ab938e0e3
phoenix <felix@feldspaten.org>
2018-04-26 16:33:01 +0200

    Added OpenMPI

--- packages/hdf5.sh
+++ packages/hdf5.sh
@@ -6,11 +6,10 @@ FILE="$PACKAGE.tar.gz"
 URL="https://support.hdfgroup.org/ftp/HDF5/current18/src/$FILE"
 SHA256SUM="6ed660ccd2bc45aa808ea72e08f33cc64009e9dd4e3a372b53438b210312e8d9"
 
-# Consider adding szip 2.1.1 and zlib
-DEPENDS=""
+DEPENDS="zlib"
 
-export LD_RUN_PATH=""
+export LD_RUN_PATH="$ZLIB_PKG/lib"
 
 pkg_configure() {
-	./configure --prefix="$DIFAS_PREFIX/$PACKAGE" #--enable-parallel
+	./configure --prefix="$DIFAS_PREFIX/$PACKAGE" --with-zlib=$ZLIB_PKG
 }
---
+++ packages/openmpi.sh
@@ -0,0 +1,17 @@
+NAME="openmpi"
+VERSION="3.0.1"
+PACKAGE="$NAME-$VERSION"
+
+FILE="$PACKAGE.tar.bz2"
+URL="https://www.open-mpi.org/software/ompi/v3.0/downloads/$FILE"
+SHA256SUM="663450d1ee7838b03644507e8a76edfb1fba23e601e9e0b5b2a738e54acd785d"
+
+DEPENDS=""
+
+export LD_RUN_PATH=""
+
+pkg_configure() {
+	./configure --prefix="$DIFAS_PREFIX/$PACKAGE"
+}
+
+
