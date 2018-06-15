ea6ab69e27d9382ad5b2c5f1cbd46ade136b33a3
Alex Hirsch <w4rh4wk@bluephoenix.at>
2018-06-15 15:23:05 +0200

    Add mcpp

---
+++ packages/mcpp.sh
@@ -0,0 +1,7 @@
+NAME="mcpp"
+VERSION="2.7.2"
+PACKAGE="$NAME-$VERSION"
+
+FILE="$PACKAGE.tar.gz"
+URL="https://downloads.sourceforge.net/project/mcpp/mcpp/V.$VERSION/$FILE"
+SHA256SUM="3b9b4421888519876c4fc68ade324a3bbd81ceeb7092ecdbbc2055099fcb8864"
